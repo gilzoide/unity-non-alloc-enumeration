33bb298ab76b48f51f72ed375bc250cad7897401
gilzoide <gilzoide@gmail.com>
2023-05-27 13:27:10 -0300

    Add enumerables for GetComponent* methods that use pooled lists
    This only avoids allocations on repeated calls. People can manually
    create the lists and use them for getting components, but these
    extension methods make for a simpler idiom, if components are only to be
    enumerated.

---
+++ Runtime/ComponentEnumeration.cs
@@ -0,0 +1,90 @@
+#if UNITY_2021_1_OR_NEWER
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Rendering;
+
+namespace Gilzoide.NonAllocEnumeration
+{
+    public static class ComponentEnumeration
+    {
+        public static NonAllocEnumerable<PooledListEnumerator<T>, T> EnumerateComponents<T>(this GameObject gameObject)
+        {
+            List<T> list = ListPool<T>.Get();
+            gameObject.GetComponents(list);
+            return new NonAllocEnumerable<PooledListEnumerator<T>, T>(new PooledListEnumerator<T>(list));
+        }
+
+        public static NonAllocEnumerable<PooledListEnumerator<T>, T> EnumerateComponents<T>(this Component component)
+        {
+            return component.gameObject.EnumerateComponents<T>();
+        }
+
+#if UNITY_5_3_OR_NEWER
+        public static NonAllocEnumerable<PooledListEnumerator<T>, T> EnumerateComponentsInChildren<T>(this GameObject gameObject, bool includeInactive = false)
+        {
+            List<T> list = ListPool<T>.Get();
+            gameObject.GetComponentsInChildren(includeInactive, list);
+            return new NonAllocEnumerable<PooledListEnumerator<T>, T>(new PooledListEnumerator<T>(list));
+        }
+
+        public static NonAllocEnumerable<PooledListEnumerator<T>, T> EnumerateComponentsInChildren<T>(this Component component, bool includeInactive = false)
+        {
+            return component.gameObject.EnumerateComponentsInChildren<T>(includeInactive);
+        }
+
+        public static NonAllocEnumerable<PooledListEnumerator<T>, T> EnumerateComponentsInParent<T>(this GameObject gameObject, bool includeInactive = false)
+        {
+            List<T> list = ListPool<T>.Get();
+            gameObject.GetComponentsInParent(includeInactive, list);
+            return new NonAllocEnumerable<PooledListEnumerator<T>, T>(new PooledListEnumerator<T>(list));
+        }
+
+        public static NonAllocEnumerable<PooledListEnumerator<T>, T> EnumerateComponentsInParent<T>(this Component component, bool includeInactive = false)
+        {
+            return component.gameObject.EnumerateComponentsInParent<T>(includeInactive);
+        }
+#endif
+    }
+
+    public struct PooledListEnumerator<T> : IEnumerator<T>
+    {
+        private List<T> _list;
+        private int _index;
+
+        public PooledListEnumerator(List<T> list)
+        {
+            _list = list;
+            _index = -1;
+        }
+
+        public T Current => _list[_index];
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            ListPool<T>.Release(_list);
+        }
+
+        public bool MoveNext()
+        {
+            int newIndex = _index + 1;
+            if (newIndex >= 0 && newIndex < _list.Count)
+            {
+                _index = newIndex;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+    }
+}
+#endif
